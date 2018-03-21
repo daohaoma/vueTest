f4ce9f6a1cdb29766f133cfc921666da42d00f52
QIAOFANG\jianhua.zhu <m18375326020_1@163.com>
2018-03-21 19:10:24 +0800

    这是一个vue.js购物车案例

---
+++ first_vue_project/src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '../components/HelloWorld'
+import Cart from '../components/cart.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/HelloWorld',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/Cart',
+      name: 'Cart',
+      component: Cart
+    }
+  ]
+})
