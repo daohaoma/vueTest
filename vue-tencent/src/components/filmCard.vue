026189d4e3b5c9b76b9eac189dc3f5453d2f0544
QIAOFANG\jianhua.zhu <m18375326020_1@163.com>
2018-04-04 18:12:32 +0800

    feat: add movie card

---
+++ vue-tencent/src/components/filmCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card-wrap">
+    <div class="card-content" v-for='(item, index) in movieList' :key='index'>
+      <div class="film-img">
+        <img :src='item.images.small' :alt='item.title'>
+      </div>
+      <div class="film-content"></div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    data() {},
+    props: {
+      movieList: {
+        type: Array,
+        default: [],
+      },
+    },
+  }
+</script>
+<style>
+  @import url('../assets/css/filmCard.css');
+</style>
+
--- vue-tencent/src/pages/user.vue
+++ vue-tencent/src/pages/user.vue
@@ -1,20 +1,31 @@
 <template>
-  <div>你好</div>
+  <div class="content">
+    <h1>TOP250电影排名</h1>
+    <div class="film-card">12</div>
+  </div>
 </template>
 
 <script>
   export default {
     name: 'user',
     data() {
-      return{}
+      return{
+        movieList: [],
+        start: 0,
+        count: 10
+      }
     },
     mounted: function() {
       this.userShow()
     },
     methods: {
       userShow: function() {
+        // this.$http.jsonp('https://api.douban.com/v2/movie/top250?start=this.start&count=this.count', {},{ headers: {}, emulateJSON: true }).then((response) => {
+        //   console.log(response)
+        //   console.log(response.data)
+        //   this.movieList = response.data.subjects
+        // })
         console.log('12')
-        // this.$http.get().then((response) => {})
       },
     },
   }
