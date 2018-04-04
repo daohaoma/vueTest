82356a95f0ada2b8cc7483c50bc0d617665ce581
QIAOFANG\jianhua.zhu <m18375326020_1@163.com>
2018-04-04 14:48:12 +0800

    feat: add tencent

---
+++ vue-tencent/src/pages/user.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>你好</div>
+</template>
+
+<script>
+  export default {
+    name: 'user',
+    data() {
+      return{}
+    },
+    mounted: function() {
+      this.userShow()
+    },
+    methods: {
+      userShow: function() {
+        console.log('12')
+        // this.$http.get().then((response) => {})
+      },
+    },
+  }
+</script>
+
---
+++ vue-tencent/src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import User from '../pages/user.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/user',
+      name: 'User',
+      component: User
+    },
+  ]
+})
